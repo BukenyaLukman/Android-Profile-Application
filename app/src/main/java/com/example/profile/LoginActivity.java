a730484203c07623f720ae2cedbf8220c191f23e
BukenyaLukman <bukenyalukman@gmail.com>
2020-08-14 22:57:58 +0300

    Registration Activity and animations

--- app/src/main/java/com/example/profile/LoginActivity.java
+++ app/src/main/java/com/example/profile/LoginActivity.java
@@ -2,16 +2,60 @@ package com.example.profile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button CallSignUp,login_btn;
+    private ImageView image;
+    private TextView logoText, sloganText;
+    TextInputLayout username,password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+
+
+        CallSignUp = findViewById(R.id.signup_screen);
+        image = findViewById(R.id.logoImage);
+        logoText = findViewById(R.id.logo_name);
+        sloganText = findViewById(R.id.slogan_name);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        login_btn = findViewById(R.id.login_btn);
+
+        CallSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                Pair[] pairs = new Pair[7];
+                pairs[0] = new Pair<View,String>(image,"logo_image");
+                pairs[1] = new Pair<View,String>(logoText,"logo_text");
+                pairs[2] = new Pair<View,String>(sloganText,"logo_desc");
+                pairs[3] = new Pair<View,String>(username,"username_tran");
+                pairs[4] = new Pair<View,String>(password,"password_tran");
+                pairs[5] = new Pair<View,String>(login_btn,"button_train");
+                pairs[6] = new Pair<View,String>(CallSignUp,"login_signup_tran");
+
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                    ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(LoginActivity.this,pairs);
+                    startActivity(intent,options.toBundle());
+                }
+
+            }
+        });
 
     }
 
